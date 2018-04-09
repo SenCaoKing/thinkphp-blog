8b7ca1e8d28d7f8a25b61280cd2182bb9549b476
SenCaoKing <1134349927@qq.com>
2018-04-09 21:42:59 +0800

    添加Data-数据处理类放

--- ThinkPHP/Library/Org/Bjy/Data.class.php
+++ ThinkPHP/Library/Org/Bjy/Data.class.php
@@ -69,9 +69,148 @@ final class Data
         return $category;
     }
 
+    // 只供channelList方法使用
+    static private function _channelList($data, $pid = 0, $html = "&nbsp;", $fieldPri = 'cid', $fieldPid = 'pid', $level = 1)
+    {
+        if (empty($data))
+            return array();
+        $arr = array();
+        foreach ($data as $v) {
+            $id = $v[$fieldPri];
+            if ($v[$fieldPid] == $pid) {
+                $v['_level'] = $level;
+                $v['_html'] = str_repeat($html, $level - 1);
+                array_push($arr, $v);
+                $tmp = self::_channelList($data, $id, $html, $fieldPri, $fieldPid, $level + 1);
+                $arr = array_merge($arr, $tmp);
+            }
+        }
+        return $arr;
+    }
+
+    /**
+     * 获得树状数据
+     * @param         $data     数据
+     * @param         $title    字段名
+     * @param  string $fieldPri 主键id
+     * @param  string $fieldPid 父id
+     * @return array
+     */
+    static public function tree($data, $title, $fieldPri = 'cid', $fieldPid = 'pid')
+    {
+        if（!is_array($data) || empty($data)
+            return array();
+        $arr = Data::channelList($data, 0, '', $fieldPri, $fieldPid);
+        foreach ($arr as $k => $v) {
+            $atr = "";
+            if ($v['_level'] > 2) {
+                for ($i = 1, $i < $v['_level'] - 1; $i++) {
+                    $str .= "|&nbsp;&nbsp;&nbsp;&nbsp;";
+                }
+            }
+            if ($v['_level'] != 1) {
+                $t = $title ? $v[$title] : "";
+                if (isset($arr[$k + 1]) && $arr[$k + 1]['_level'] >= $arr[$k]['_level']) {
+                    $arr[$k]['_name'] = $str . "├─" . $v['_html'] . $t;
+                } else {
+                    $arr[$k]['_name'] = $str . "└─" . $v['_html'] . $t;
+                }
+            } else {
+                $arr[$k]['_name'] = $v[$title];
+            }
+        }
+        // 设置主键为$fieldPri
+        $data = array();
+        foreach ($arr as $d) {
+            $data[$d[$fieldPri]] = $d;
+        }
+        return $data;
+    }
+
+    /**
+     * 获得所有父级栏目
+     * @param         $data     栏目数据
+     * @param         $sid      子栏目
+     * @param  string $fieldPri 唯一键名，如果是表则是表的主键
+     * @param  string $fieldPid 父id键名
+     * @return array
+     */
+    static public function parentChannel($data, $sid, $fieldPri = 'cid', $fieldPid = 'pid')
+    {
+        if (empty($data)) {
+            return $data;
+        } else {
+            $arr = array();
+            foreach ($data as $v) {
+                if ($v[$fieldPri] == $sid) {
+                    $arr[] = $v;
+                    $_n = self::parentChannel($data, $v[$fieldPid], $fieldPri, $fieldPid);
+                    if (!empty($_n)) {
+                        $arr = array_merge($arr, $_n);
+                    }
+                }
+            }
+            return $arr;
+        }
+    }
+
+    /**
+     * 判断$s_cid是否是$d_cid的子栏目
+     * @param          $data     栏目数据
+     * @param          $sid      子栏目id
+     * @param          $pid      父栏目id
+     * @param  string  $fieldPri 主键
+     * @param  string  $fieldPid 父id字段
+     * @return boolean
+     */
+    static public function isChild($data, $sid, $pid, $fieldPri = 'cid', $fieldPid = 'pid')
+    {
+        $_data = self::channelList($data, $pid, '', $fieldPri, $fieldPid);
+        foreach ($_data as $c) {
+            // 目标栏目为原栏目的子栏目
+            if ($c[$fieldPri] == $sid)
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * 检测是否有子栏目
+     * @param          $data     栏目数据
+     * @param          $cid      要判断的栏目cid
+     * @param  string  $fieldPid 父id表字段名
+     * @return boolean
+     */
+    static function hasChild($data, $cid, $fieldPid = 'pid')
+    {
+        foreach ($data as $d) {
+            if ($d[$fieldPid] == $cid) return true;
+        }
+        return false;
+    }
+
+    /**
+     * 递归实现笛卡尔乘积
+     * @param         $arr 操作的数组
+     * @param  array  $tmp
+     * @return array
+     */
+    static function descarte($arr, $tmp = array())
+    {
+        static $n_arr = array();
+        foreach (array_shift($arr) as $v) {
+            $tmp[] = $v;
+            if ($arr) {
+                self::descarte($arr, $tmp);
+            } else {
+                $_arr[] = $tmp;
+            }
+            array_pop($tmp);
+        }
+        return $n_arr;
+    }
 
 
-    
        
 }
 
