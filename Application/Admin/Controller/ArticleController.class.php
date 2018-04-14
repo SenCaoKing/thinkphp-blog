ad0f457c3f829d8078edaa0a6f770495f3c16613
SenCaoKing <1134349927@qq.com>
2018-04-14 15:14:58 +0800

    完成文章修改

--- Application/Admin/Controller/ArticleController.class.php
+++ Application/Admin/Controller/ArticleController.class.php
@@ -45,7 +45,11 @@ class ArticleController extends AuthController{
     // 修改文章
     public function edit(){
         if(IS_POST){
-
+            if($this->db->editData()){
+                $this->success('修改成功');
+            }else{
+                $this->error('修改失败');
+            }
         }else{
             $aid=I('aid');
             $data=$this->db->getDataByAid($aid);
--- Application/Common/Model/ArticleTagModel.class.php
+++ Application/Common/Model/ArticleTagModel.class.php
@@ -3,7 +3,6 @@ namespace Common\Model;
 use Think\Model;
 
 class ArticleTagModel extends Model{
-
     /**
      * 添加数据
      * @param string $aid  文章id
@@ -25,6 +24,7 @@ class ArticleTagModel extends Model{
         $this->where("aid=$aid")->delete();
         return true;
     }
+
     // 传递aid获取tid数组
     public function getDataByAid($aid){
         return $this->where("aid=$aid")->getField('tid',true);
