25b6329e7de3c4ea2594ff099a4f83806fb6b294
SenCaoKing <1134349927@qq.com>
2018-04-15 11:30:26 +0800

    升级bootstrap3.3.2到3.3.4

---
+++ Runtime/Cache/Home/2a94b8bda04ff446dcedd42276fc99ee.php
@@ -0,0 +1,46 @@
+<?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <title>Sen-Blog</title>
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+<script type="text/javascript" src="/Public/static/js/jquery-2.0.0.min.js"></script>
+<link rel="stylesheet" type="text/css" href="/Public/static/bootstrap-3.3.4/css/bootstrap.min.css" />
+<link rel="stylesheet" type="text/css" href="/Public/static/bootstrap-3.3.4/css/bootstrap-theme.min.css" />
+<link rel="stylesheet" type="text/css" href="/Public/static/font-awesome-4.3.0/css/font-awesome.min.css" />
+<!--[if lt IE 9]>
+<script type="text/javascript" src="/Public/static/js/html5shiv.min.js"></script>
+<script type="text/javascript" src="/Public/static/js/respond.min.js"></script>
+<![endif]-->
+<link rel="stylesheet" type="text/css" href="/Public/static/css/bjy.css" />
+<script type="text/javascript" src="/Public/static/bootstrap-3.3.4/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="/Public/static/iCheck-1.0.2/icheck.min.js"></script>
+<link rel="stylesheet" href="/Public/static/iCheck-1.0.2/skins/all.css" />
+<script>
+$(document).ready(function(){
+    $('.icheck').iCheck({
+        checkboxClass: "icheckbox_square-blue",
+        radioClass: "iradio_square-blue",
+        increaseArea: 20%
+    });
+});
+</script>
+<link rel="stylesheet" href="/Public/Home/css/index.css" />
+</head>
+<body>
+    <!-- 顶部导航开始 -->
+    <div id="nav">
+        <div class="b-inside">
+            <div class="logo">Sen个人博客</div>
+            <ul class="category">
+                <li class="cname action">首页</li>
+                <li class="cname">分类1</li>
+                <li class="cname">分类2</li>
+            </ul>
+        </div>
+    </div>
+
+
+    
+</body>
+</html>
